478a1b33d474738fa696d148ac34707f61294bdf
Jesse Lucas <jessejlucas@hotmail.com>
2025-12-05 14:42:57 +0800

    Refactor station list to remove synthwave and ensure consistency in station URLs

--- stations.go
+++ stations.go
@@ -6,9 +6,16 @@ import (
 )
 
 var stations = map[string]string{
-	"lofi-girl":      "https://play.streamafrica.net/lofiradio",
-	"chilled-cow":    "https://stream.zeno.fm/0r0xa792kwzuv",
-	"lofi-synthwave": "https://stream.zeno.fm/f3wvbbqmdg8uv",
+	"lofi-girl":    "https://play.streamafrica.net/lofiradio",
+	"chilled-cow":  "https://stream.zeno.fm/0r0xa792kwzuv",
+	"boxlofi":      "https://boxradio-edge-00.streamafrica.net/lofi",
+	"the-chillout": "https://streams.radiomast.io/chill-out",
+	"smooth-jazz":  "https://mediagw.e-tiger.net/stream/zc07",
+	"deep-focus":   "https://stream-uk1.radioparadise.com/mellow-320",
+	"groove-salad": "https://ice1.somafm.com/groovesalad-128-mp3",
+	"drone-zone":   "https://ice1.somafm.com/dronezone-128-mp3",
+	"deep-space":   "https://ice1.somafm.com/deepspaceone-128-mp3",
+	"defcon":       "https://ice1.somafm.com/defcon-128-mp3",
 }
 
 func ListStations() {
